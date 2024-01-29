4f0929453b24d77b856e9237458fa4a841c2c7d6
linpoyu0415 <linpoyu0415@gmail.com>
2024-01-29 17:08:43 +0800

    Commit day01 homework for Lucky

---
+++ lucky/day01/vue391-lab/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <button @click="onClick">+</button>
+  <div>{{ count }}</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+let count = ref(0)
+let onClick = () => count.value++
+</script>
